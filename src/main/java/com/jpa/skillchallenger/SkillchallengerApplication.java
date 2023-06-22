8eadcbf4ef14a1e194f1f7a18442364bff089dc7
back-moon <anstmdwn2000@gmail.com>
2023-06-23 01:34:14 +0900

    초기 버전

---
+++ src/main/java/com/jpa/skillchallenger/SkillchallengerApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.skillchallenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillchallengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillchallengerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jpa/skillchallenger/SkillchallengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.skillchallenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillchallengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
